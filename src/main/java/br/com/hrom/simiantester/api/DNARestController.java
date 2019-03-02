6038834aafcd7a4a605ae4ba2124ec3a01075a9d
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-02 20:15:46 -0300

    default exception handler

--- src/main/java/br/com/hrom/simiantester/api/DNARestController.java
+++ src/main/java/br/com/hrom/simiantester/api/DNARestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class DNARestController {
 
@@ -22,7 +24,7 @@ public class DNARestController {
 
 
     @PostMapping(path = "/simian", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity isSimian(@RequestBody DNARequest request) {
+    public ResponseEntity isSimian(@Valid @RequestBody DNARequest request) {
         boolean isSimian;
 
         try {
--- src/main/java/br/com/hrom/simiantester/api/DefaultExceptionHandler.java
+++ src/main/java/br/com/hrom/simiantester/api/DefaultExceptionHandler.java
@@ -1,4 +1,23 @@
 package br.com.hrom.simiantester.api;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class DefaultExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleNormalException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<String> messages = fieldErrors.stream()
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ErrorResponse(messages);
+    }
 }
