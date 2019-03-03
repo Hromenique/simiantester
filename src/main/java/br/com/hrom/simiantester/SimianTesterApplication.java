f3c3ece0e6b5c1e97118d0b8f9f01fc3bba0a81e
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-03 17:14:52 -0300

    configurações de conexão com o mongo, rename da Classe que representa a app, configurações de CORS

--- src/main/java/br/com/hrom/simiantester/SimianTesterApplication.java
+++ src/main/java/br/com/hrom/simiantester/SimianTesterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimiantesterApplication {
+public class SimianTesterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimiantesterApplication.class, args);
+		SpringApplication.run(SimianTesterApplication.class, args);
 	}
 
 }
--- src/test/java/br/com/hrom/simiantester/SimianTesterApplicationTests.java
+++ src/test/java/br/com/hrom/simiantester/SimianTesterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SimiantesterApplicationTests {
+public class SimianTesterApplicationTests {
 
 	@Test
 	public void contextLoads() {
